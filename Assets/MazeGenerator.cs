7f948ad87d4986e9eb3ccd6a669a9c8b6575ddf4
het1309 <hetpatel139@gmail.com>
2024-04-14 16:57:10 -0500

    Set up Unity project

---
+++ Assets/MazeGenerator.cs
@@ -0,0 +1,284 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    [SerializeField] MazeNode nodePrefab;
+    [SerializeField] Vector2Int mazeSize;
+    [SerializeField] float nodeSize;
+    [SerializeField] GameObject spherePrefab;
+
+    private MazeNode lastRedNode;
+    
+    private GameObject sphere;
+    
+    private void Start()
+    {
+        GenerateMazeInstant(mazeSize);
+        //StartCoroutine(GenerateMaze(mazeSize));
+        SetSpherePositionRandomly();
+    }
+
+    void GenerateMazeInstant(Vector2Int size)
+    {
+        List<MazeNode> nodes = new List<MazeNode>();
+
+        // Create nodes
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                //Vector3 nodePos = new Vector3(x - (size.x / 2f), 0, y - (size.y / 2f));
+                Vector3 nodePos = new Vector3(x + (nodeSize / 2f), 0, y + (nodeSize / 2f));
+                MazeNode newNode = Instantiate(nodePrefab, nodePos, Quaternion.identity, transform);
+                nodes.Add(newNode);
+                if (newNode.GetState() == NodeState.End)
+                {
+                    lastRedNode = newNode; // Store the last red node
+                }
+            }
+        }
+
+        List<MazeNode> currentPath = new List<MazeNode>();
+        List<MazeNode> completedNodes = new List<MazeNode>();
+
+        // Choose starting node
+        currentPath.Add(nodes[Random.Range(0, nodes.Count)]);
+        currentPath[0].SetState(NodeState.Current);
+        
+        while (completedNodes.Count < nodes.Count)
+        {
+            // Check nodes next to the current node
+            List<int> possibleNextNodes = new List<int>();
+            List<int> possibleDirections = new List<int>();
+
+            int currentNodeIndex = nodes.IndexOf(currentPath[currentPath.Count - 1]);
+            int currentNodeX = currentNodeIndex / size.y;
+            int currentNodeY = currentNodeIndex % size.y;
+
+            if (currentNodeX < size.x - 1)
+            {
+                // Check node to the right of the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex + size.y]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex + size.y]))
+                {
+                    possibleDirections.Add(1);
+                    possibleNextNodes.Add(currentNodeIndex + size.y);
+                }
+            }
+            if (currentNodeX > 0)
+            {
+                // Check node to the left of the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex - size.y]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex - size.y]))
+                {
+                    possibleDirections.Add(2);
+                    possibleNextNodes.Add(currentNodeIndex - size.y);
+                }
+            }
+            if (currentNodeY < size.y - 1)
+            {
+                // Check node above the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex + 1]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex + 1]))
+                {
+                    possibleDirections.Add(3);
+                    possibleNextNodes.Add(currentNodeIndex + 1);
+                }
+            }
+            if (currentNodeY > 0)
+            {
+                // Check node below the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex - 1]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex - 1]))
+                {
+                    possibleDirections.Add(4);
+                    possibleNextNodes.Add(currentNodeIndex - 1);
+                }
+            }
+
+            // Choose next node
+            if (possibleDirections.Count > 0)
+            {
+                int chosenDirection = Random.Range(0, possibleDirections.Count);
+                MazeNode chosenNode = nodes[possibleNextNodes[chosenDirection]];
+
+                switch (possibleDirections[chosenDirection])
+                {
+                    case 1:
+                        chosenNode.RemoveWall(1);
+                        currentPath[currentPath.Count - 1].RemoveWall(0);
+                        break;
+                    case 2:
+                        chosenNode.RemoveWall(0);
+                        currentPath[currentPath.Count - 1].RemoveWall(1);
+                        break;
+                    case 3:
+                        chosenNode.RemoveWall(3);
+                        currentPath[currentPath.Count - 1].RemoveWall(2);
+                        break;
+                    case 4:
+                        chosenNode.RemoveWall(2);
+                        currentPath[currentPath.Count - 1].RemoveWall(3);
+                        break;
+                }
+
+                currentPath.Add(chosenNode);
+                chosenNode.SetState(NodeState.Current);
+            }
+            else
+            {
+                // Check if it's the last node in the current path
+                if (currentPath.Count == 1)
+                {
+                    // Handle the last node (set a different material, etc.)
+                    completedNodes.Add(currentPath[currentPath.Count - 1]);
+                    currentPath[currentPath.Count - 1].SetState(NodeState.End);
+                }
+                else
+                {
+                    // Not the last node, proceed as usual
+                    completedNodes.Add(currentPath[currentPath.Count - 1]);
+                    currentPath[currentPath.Count - 1].SetState(NodeState.Completed);
+                    currentPath.RemoveAt(currentPath.Count - 1);
+                }
+            }
+        }
+    }
+    void SetSpherePositionRandomly()
+    {
+        // Instantiate the sphere prefab
+        GameObject sphere = Instantiate(spherePrefab, Vector3.zero, Quaternion.identity);
+        sphere.AddComponent<SphereCollider>().isTrigger = true;
+
+        // Generate random coordinates within the bounds of the maze
+        float randomX = UnityEngine.Random.Range(0, mazeSize.x) + (nodeSize / 2f);
+        float randomZ = UnityEngine.Random.Range(0, mazeSize.y) + (nodeSize / 2f);
+
+        // Set the position of the sphere to the randomly generated coordinates
+        sphere.transform.position = new Vector3(randomX, 0, randomZ);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == sphere && lastRedNode != null)
+        {
+            Debug.Log("Sphere touched the last red node!");
+            // Trigger any desired action here
+        }
+    }
+    IEnumerator GenerateMaze(Vector2Int size)
+    {
+        List<MazeNode> nodes = new List<MazeNode>();
+
+        // Create nodes
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                Vector3 nodePos = new Vector3(x - (size.x / 2f), 0, y - (size.y / 2f));
+                MazeNode newNode = Instantiate(nodePrefab, nodePos, Quaternion.identity, transform);
+                nodes.Add(newNode);
+
+                yield return null;
+            }
+        }
+
+        List<MazeNode> currentPath = new List<MazeNode>();
+        List<MazeNode> completedNodes = new List<MazeNode>();
+
+        // Choose starting node
+        currentPath.Add(nodes[Random.Range(0, nodes.Count)]);
+        currentPath[0].SetState(NodeState.Current);
+
+        while (completedNodes.Count < nodes.Count)
+        {
+            // Check nodes next to the current node
+            List<int> possibleNextNodes = new List<int>();
+            List<int> possibleDirections = new List<int>();
+
+            int currentNodeIndex = nodes.IndexOf(currentPath[currentPath.Count - 1]);
+            int currentNodeX = currentNodeIndex / size.y;
+            int currentNodeY = currentNodeIndex % size.y;
+
+            if (currentNodeX < size.x - 1)
+            {
+                // Check node to the right of the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex + size.y]) && 
+                    !currentPath.Contains(nodes[currentNodeIndex + size.y]))
+                {
+                    possibleDirections.Add(1);
+                    possibleNextNodes.Add(currentNodeIndex + size.y);
+                }
+            }
+            if (currentNodeX > 0)
+            {
+                // Check node to the left of the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex - size.y]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex - size.y]))
+                {
+                    possibleDirections.Add(2);
+                    possibleNextNodes.Add(currentNodeIndex - size.y);
+                }
+            }
+            if (currentNodeY < size.y - 1)
+            {
+                // Check node above the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex + 1]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex + 1]))
+                {
+                    possibleDirections.Add(3);
+                    possibleNextNodes.Add(currentNodeIndex + 1);
+                }
+            }
+            if (currentNodeY > 0)
+            {
+                // Check node below the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex - 1]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex - 1]))
+                {
+                    possibleDirections.Add(4);
+                    possibleNextNodes.Add(currentNodeIndex - 1);
+                }
+            }
+
+            // Choose next node
+            if (possibleDirections.Count > 0)
+            {
+                int chosenDirection = Random.Range(0, possibleDirections.Count);
+                MazeNode chosenNode = nodes[possibleNextNodes[chosenDirection]];
+
+                switch (possibleDirections[chosenDirection])
+                {
+                    case 1:
+                        chosenNode.RemoveWall(1);
+                        currentPath[currentPath.Count - 1].RemoveWall(0);
+                        break;
+                    case 2:
+                        chosenNode.RemoveWall(0);
+                        currentPath[currentPath.Count - 1].RemoveWall(1);
+                        break;
+                    case 3:
+                        chosenNode.RemoveWall(3);
+                        currentPath[currentPath.Count - 1].RemoveWall(2);
+                        break;
+                    case 4:
+                        chosenNode.RemoveWall(2);
+                        currentPath[currentPath.Count - 1].RemoveWall(3);
+                        break;
+                }
+
+                currentPath.Add(chosenNode);
+                chosenNode.SetState(NodeState.Current);
+            }
+            else
+            {
+                completedNodes.Add(currentPath[currentPath.Count - 1]);
+
+                currentPath[currentPath.Count - 1].SetState(NodeState.Completed);
+                currentPath.RemoveAt(currentPath.Count - 1);
+            }
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
